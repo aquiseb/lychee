cd54e6bd34170b1267b8674b6cc286a8dbeb616f
Mies <hello@asten.io>
2019-09-24 00:15:22 +0200

    Demo data seed

--- core/mongo.go
+++ core/mongo.go
@@ -2,8 +2,14 @@ package core
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"math/rand"
+	"strings"
+	"time"
 
+	"github.com/spf13/viper"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -11,7 +17,9 @@ import (
 
 // GetClient returns a MongoDB Client
 func GetClient() (*mongo.Client, error) {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	uri := viper.GetString("db.uri")
+
+	clientOptions := options.Client().ApplyURI(uri)
 	client, err := mongo.NewClient(clientOptions)
 	if err != nil {
 		log.Fatal(err)
@@ -24,9 +32,63 @@ func GetClient() (*mongo.Client, error) {
 	err = client.Ping(context.Background(), readpref.Primary())
 	if err != nil {
 		log.Fatal("Couldn't connect to the database", err)
-	} else {
-		log.Println("Mongo connected")
 	}
 
+	fmt.Println("Mongo connected")
+
 	return client, nil
+}
+
+// FindOrSeed checks if the database was already seeded or seeds it
+func FindOrSeed(collection *mongo.Collection, key string, data []interface{}) {
+	dbName := viper.GetString("db.name")
+
+	// Get a random element in data
+	rand.Seed(time.Now().Unix())
+	n := rand.Int() % len(data)
+	filter := data[n]
+
+	// Find the random element in the database
+	var result interface{}
+	err := collection.FindOne(context.TODO(), filter).Decode(&result)
+
+	if err != nil {
+		// Insert the data only if the random element was not found in the database
+		if strings.Contains(err.Error(), "no documents in result") {
+			_, err = collection.InsertMany(context.TODO(), data)
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Println("DB", dbName, "contains", len(data), "demo", key)
+
+}
+
+// Seed inserts some demo data into the database
+func Seed(c *mongo.Client) {
+	dbName := viper.GetString("db.name")
+	col := c.Database(dbName)
+
+	dataToSeed := map[string][]interface{}{
+		"users": {
+			bson.M{"id": "1", "firstname": "Bob", "lastname": "Dylan"},
+			bson.M{"id": "2", "firstname": "Johnny", "lastname": "Cash"},
+			bson.M{"id": "3", "firstname": "Elvis", "lastname": "Presley"},
+		},
+		"posts": {
+			bson.M{"id": "1", "title": "First post", "slug": "first-post"},
+			bson.M{"id": "2", "title": "Second post", "slug": "second-post"},
+			bson.M{"id": "3", "title": "Third post", "slug": "third-post"},
+		},
+	}
+
+	for key, data := range dataToSeed {
+		collection := col.Collection(key)
+		FindOrSeed(collection, key, data)
+	}
+
 }
