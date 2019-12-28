8b5abca43eee5957fcff745d529482080e84aec5
Asten Mies <sebastien@dolbecke.com>
2019-12-28 13:29:41 +0100

    ok

--- micro-post/schema/bindata.go
+++ micro-post/schema/bindata.go
@@ -83,7 +83,7 @@ func schemaSchemaGraphql() (*asset, error) {
 		return nil, err
 	}
 
-	info := bindataFileInfo{name: "schema/schema.graphql", size: 587, mode: os.FileMode(420), modTime: time.Unix(1577137632, 0)}
+	info := bindataFileInfo{name: "schema/schema.graphql", size: 587, mode: os.FileMode(420), modTime: time.Unix(1577137647, 0)}
 	a := &asset{bytes: bytes, info: info}
 	return a, nil
 }
